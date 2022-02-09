93c8bca66a6fec9f88258aee5963a14c227e77ae
Ruth Gabriel de Sousa <ruth.sousa15@gmail.com>
2022-02-08 21:31:59 -0700

    Remove whitespace

--- calc.js
+++ calc.js
@@ -7,12 +7,10 @@ const reader = readline.createInterface({
 
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
-	
+
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
-
-	
 
 	if( mathSymbol === "+"){
 		console.log(num1+num2);
@@ -29,8 +27,7 @@ reader.question("What would you like to calculate?", function(input){
 	else if( mathSymbol === "sqrt"){
 		console.log(Math.sqrt(num1));
 	}
-	
-		
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 });
