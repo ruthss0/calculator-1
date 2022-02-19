ca9193cbf842495241924b2c6f56b34151679047
ruthss0 <82294375+ruthss0@users.noreply.github.com>
2022-02-19 15:04:39 -0700

    Update calc.js

--- calc.js
+++ calc.js
@@ -25,7 +25,7 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(num1+num2);
 	}
 	else if( mathSymbol === "sqrt"){
-		console.log(Math.sqrt(num1));
+		console.log(Math.sqrt(num1, num2));
 	}
 
 	// This line closes the connection to the command line interface.
